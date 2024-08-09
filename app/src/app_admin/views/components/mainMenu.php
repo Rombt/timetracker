ed2a9c5bcb368be132633eca1cbf75cea79636be
Rombt <rombtnet@gmail.com>
2024-08-09 11:13:07 +0300

    overflow-cont and icon-overflow

--- app/src/app_admin/views/components/mainMenu.php
+++ app/src/app_admin/views/components/mainMenu.php
@@ -1,10 +1,11 @@
 <div class="cont-main-horizontal-menu">
-   <nav class="main-horizontal-menu">
-      <ul>
-         <li><a href="/">home</a></li>
-         <li><a href="/">support</a></li>
-         <li><a href="/">about</a></li>
-         <li><a href="/">login</a></li>
-      </ul>
-   </nav>
+	<nav class="main-horizontal-menu">
+		<ul>
+			<li><a href="/">home</a></li>
+			<li><a href="/">support</a></li>
+			<li><a href="/">about</a></li>
+			<li><a href="/">profile</a></li>
+			<li><a href="/">login</a></li>
+		</ul>
+	</nav>
 </div>
