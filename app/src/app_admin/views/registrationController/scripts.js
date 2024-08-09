89cce1198d56ab8243e144fd50d4208ea6ad190b
Rombt <rombtnet@gmail.com>
2024-08-09 16:45:12 +0300

    .

--- app/src/app_admin/views/registrationController/scripts.js
+++ app/src/app_admin/views/registrationController/scripts.js
@@ -1,20 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
   const formLogin = document.querySelector('#regForm');
-
   const fieldPass = formLogin.querySelector('#password');
   const fieldConfirmPass = formLogin.querySelector('#passwordConfirm');
-
-  console.log('fieldPass = ', fieldPass);
-  console.log('fieldConfirmPass = ', fieldConfirmPass);
-
   const submitButton = formLogin.querySelector('#submitRegForm');
   const errConfPass = formLogin.querySelector('.err-conf-pass');
 
   fieldConfirmPass.addEventListener('blur', function () {
-    const fieldPassValue = fieldPass.value;
-    const fieldConfirmPassValue = fieldConfirmPass.value;
-
-    if (fieldPassValue === fieldConfirmPassValue) {
+    if (fieldPass.value === fieldConfirmPass.value) {
       submitButton.disabled = false;
       if (errConfPass.style.display === 'block') {
         errConfPass.style.display = 'none';
