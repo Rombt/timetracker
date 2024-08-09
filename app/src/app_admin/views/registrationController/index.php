8314357a28e0eedbc6ad7daf310f856861f701dd
Rombt <rombtnet@gmail.com>
2024-08-09 15:18:52 +0300

    .

--- app/src/app_admin/views/registrationController/index.php
+++ app/src/app_admin/views/registrationController/index.php
@@ -3,44 +3,46 @@
 
 <main>
 
-   <div class="wrapper-section">
-      <div class="rmbt-full-width login-form-full-width">
-         <section class="rmbt-container login-form">
-            <div class="login-form__row">
+	<div class="wrapper-section">
+		<div class="rmbt-full-width login-form-full-width">
+			<section class="rmbt-container login-form">
+				<div class="login-form__row">
 
-               <div class="login-form__col">
+					<div class="login-form__col">
 
-                  <div class="login-form__wrap">
-                     <form id='regForm' class="font-text simple-block " action="#" method=''>
-                        <label for="email">
-                           Login:
-                           <input type="text" id="login" name='login' required>
-                        </label>
-                        <label for="email">
-                           Email:
-                           <input type="text" id="email" name='email' required>
-                        </label>
-                        <label for="password">
-                           Password:
-                           <input type="text" id="password" name='password' required>
-                        </label>
-                        <label for="password">
-                           Password confirm:
-                           <input type="text" id="passwordConfirm" name='passwordConfirm' required>
-                        </label>
+						<h1 class='font-title'>Complete this form <br> for registration</h1>
 
-                        <button type='submit' class='font-text'> registration </button>
+						<div class="login-form__wrap">
+							<form id='regForm' class="font-text simple-block " action="#" method=''>
+								<label for="email">
+									Login:
+									<input type="text" id="login" name='login' required>
+								</label>
+								<label for="email">
+									Email:
+									<input type="text" id="email" name='email' required>
+								</label>
+								<label for="password">
+									Password:
+									<input type="text" id="password" name='password' required>
+								</label>
+								<label for="password">
+									Password confirm:
+									<input type="text" id="passwordConfirm" name='passwordConfirm' required>
+								</label>
 
-                     </form>
-                  </div>
+								<button type='submit' class='font-text'> registration </button>
+
+							</form>
+						</div>
 
 
 
-               </div>
-            </div>
-         </section>
-      </div>
-   </div>
+					</div>
+				</div>
+			</section>
+		</div>
+	</div>
 
 </main>
 
